58680bf9e347b16a0b7bf54ff6f7e5519458885d
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-08-06 23:59:47 +0530

    Update Extra.jsx

--- src/components/Extra.jsx
+++ src/components/Extra.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Card from "./Card";
 
-function Extra() {
+function Extra(props) {
   return (
-    <Card 
-    name={props.name}
-    description={props.description}
-    img={props.img}
-    />
+    <Card name={props.name} description={props.description} img={props.img} />
   );
 }
 
