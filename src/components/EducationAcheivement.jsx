82590c3292c15f2d80f223fea8194ee8b4b8075e
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-08-06 23:59:27 +0530

    Update EducationAcheivement.jsx

--- src/components/EducationAcheivement.jsx
+++ src/components/EducationAcheivement.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Card from "./Card";
 
-function EducationAcheivement(props){
+function EducationAcheivement(props) {
   return (
-    <Card 
-    name={props.name}
-    description={props.description}
-    img={props.img}
-    />
+    <Card name={props.name} description={props.description} img={props.img} />
   );
 }
 
