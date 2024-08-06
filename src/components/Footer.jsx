e1ba6ba1a5e59d2a25fa460dcb8c13a25c849d44
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-08-07 00:00:04 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div>
-      <footer><p>Copyrights @ {new Date().getUTCDate()}</p></footer>
+      <footer>
+        <p>Copyrights © {new Date().getFullYear()}</p>
+      </footer>
     </div>
   );
 }
