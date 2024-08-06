00dce11a5ae9c8bfea3040ffb295461c20fecf2a
SaiGane5 <128854913+SaiGane5@users.noreply.github.com>
2024-08-07 00:00:28 +0530

    Update Project.jsx

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,9 @@
 import React from "react";
 import Card from "./Card";
 
-function Projects() {
+function Project(props) {
   return (
-    <Card 
-    name={props.name}
-    description={props.description}
-    img={props.img}
-    />
+    <Card name={props.name} description={props.description} img={props.img} />
   );
 }
 export default Project;
